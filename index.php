c7c79ee5e33e8981b6c12052de3204484e06347e
morshinin <314159265absurdus@gmail.com>
2021-11-13 03:20:11 +0300

    Init

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Валидная верстка веб-сайтов</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link href='https://fonts.googleapis.com/css?family=EB+Garamond&amp;subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Exo+2:400,800&amp;subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+	
+	<link rel="stylesheet" href="style.css">
+	
+</head>
+<body>				
+		<div class="navbar navbar-default navbar-static">
+			<div class="container top-bar">
+				<div class="navbar-header">
+				<a href="/" class="navbar-brand logo">
+						Рефактор - верстка и интеграция
+					</a>
+				</div> <!-- navbar-header -->
+				<ul class="navbar-right list-unstyled list-inline main-contact">
+					<li>
+						<i class="fa fa-phone-square"></i>							
+						 +7 926 540 54 46
+					</li>
+					<li>
+						<i class="fa fa-skype"></i>
+						 a5405446
+					</li>
+					<li>
+						<i class="fa fa-envelope"></i>
+						<a href="mailto:skech@mail.ru"> skech@mail.ru</a>
+					</li>
+				</ul>
+			</div> <!-- container -->
+		</div>
+
+	<div class="wrap-1">
+		<div class="container">
+			<div class="row standout">
+				<div class="col-xs-6  ">
+					<h1>
+					HTML5/CSS3<br>ВЕРСТКА<br>САЙТОВ<br>
+					</h1>
+
+					<!-- <p class="lead">
+						Закажите сейчас!
+					</p> -->
+						
+				</div>	<!-- col-xs-5 col-xs-offset-1   -->
+				<div class="col-xs-4 col-xs-offset-2 main-form-wrap">
+					<form action="example.php" class="main-form">
+						<input type="text" placeholder="Имя" class="form-control">
+						<input type="text" placeholder="Email, телефон или skype" class="form-control">
+						<textarea name="message" id="message" cols="30" rows="3" class="form-control" placeholder="Сообщение"></textarea>
+						<input type="submit" value="Отправить" class="btn btn-success btn-block">
+					</form>
+				</div>
+			</div>	<!-- row standout -->
+		</div> <!-- container -->
+	</div> <!-- wrap-1 -->
+
+<!-- End promo section -->
+
+
+	<div class="container portfolio">
+
+			
+		<div class="row wow fadeInUp portfolio-item">
+			<div class="col-xs-3">
+				<div class="portfolio-title-border-top"></div>
+				<h3>
+					Верстка и интеграция в Wordpress
+				</h3>
+				<p>
+					<b>Заказчик:</b> Faktor Ручные работы
+				</p>
+				<p>
+					<b>Задача:</b> сверстать дизайн-макет из psd в html/css и интегрировать в
+					Wordpress. 
+				</p>
+				<p>
+					В результате получилась полноценная тема для Wordpress.
+					Функционал позволяет редактировать пункты меню и подменю из админ.панели.
+				</p>
+				<p>
+					Посмотрите живьем: <br>
+					<a href="faktor.spb.ru">Faktor</a>
+				</p>
+			</div>
+			<div class="col-xs-8 col-xs-offset-1 portfolio-item">
+				<img src="img/faktor.jpg" alt="" class="img-responsive">
+			</div>
+		</div>  <!-- row -->
+
+		<div class="row wow fadeInUp portfolio-item">
+			<div class="col-xs-8">
+				<img src="img/video2.jpg" alt="" class="img-responsive">
+			</div>
+			<div class="col-xs-3 col-xs-offset-1 text-right">
+				<div class="portfolio-title-border-top-2"></div>
+				<h3>
+					Верстка лэндинга
+				</h3>
+				<p>
+					<b>Заказчик:</b> Видео-студия
+				</p>
+				<p>
+					<b>Задача:</b> сверстать лэндинг из psd файла
+				</p>
+				<p>
+					Небольшой лэндинг из шести блоков с формой заказа обратного звонка.
+					В разработке использовался фреймворк Bootstrap.
+				</p>
+				<p>
+					Посмотрите живьем: <br>
+					<a href="http://79265405446.ru/video/index.html">Video-studio</a>
+				</p>
+				
+			</div>
+		</div>  <!-- row -->
+
+		<div class="row wow fadeInUp portfolio-item">
+			<div class="col-xs-3">
+				<div class="portfolio-title-border-top"></div>
+				<h3>
+					Верстка интернет-магазина
+				</h3>
+				<p>
+					<b>Заказчик:</b> Интернет-магазин садового инвентаря
+				</p>
+				<p>
+					<b>Задача:</b> сверстать сайт интернет-магазина на Bootstrap
+				</p>
+				<p>
+					Верстка главной страницы со слайдером.
+				</p>
+				<p>
+					Посмотрите живьем: <br>
+					<a href="http://79265405446.ru/ogorod27/index.html">Ogorod27</a>
+				</p>
+			</div>
+			<div class="col-xs-8 col-xs-offset-1">
+				<img src="img/ogorod-27.jpg" alt="" class="img-responsive">
+			</div>
+		</div>  <!-- row -->
+		<!-- <div class="row wow fadeInUp">
+			<h2 class="text-center">
+				Свяжитесь со мной
+			</h2>
+		</div> --> <!-- row -->
+		<div class="row aditional-form wow fadeInUp">
+			<div class="col-xs-12">
+				<form action="example.php" method="post">
+					<div class="col-xs-4">
+						<input type="text" name="email" placeholder="Имя" class="form-control">
+					</div>
+					<div class="col-xs-4">
+						<input type="text" name="message" placeholder="Email, телефон или skype" class="form-control">
+					</div>
+					<div class="col-xs-4">
+						<input type="submit" class="btn btn-block btn-success">
+					</div>
+				</form>
+			</div> <!-- col-xs-12 -->
+		</div> <!-- row -->
+		<!-- <div class="row wow fadeInUp">
+			<h2 class="text-center">
+				Обо мне
+			</h2>
+		</div> --> <!-- row -->
+		<div class="row">
+			<div class="col-xs-4 col-xs-offset-2 wow slideInLeft">
+				<img src="img/my-photos.jpg" alt="">
+			</div>
+			<div class="col-xs-4 wow slideInRight">
+				<p>
+					Меня зовут Михаил Моршинин. Я профессионально занимаюсь веб-дизайном с 2014 года. Живу в городе Мытищи московской области.
+				</p>
+				<p>
+					Мои основные принципы в работе - это максимальная чистота, валидность кода и использование самых передовых технологий.
+				</p>
+				<p>
+					Моя цель - создание быстрых и удобных сайтов и приложений.
+				</p>
+				<p>
+					Помимо работы, я люблю читать, заниматься спортом, ходить в кино и театр.
+				</p>
+			</div>
+		</div> <!-- row -->
+<!-- 		<div class="row wow fadeInUp">
+			<div class="col-xs-12">
+				<h2 class="text-center">
+					Технологии
+				</h2>
+			</div> --> <!-- col-xs-12 -->
+		<!-- </div> --> <!-- row -->
+		<div class="row technology-list wow fadeInUp">
+			<div class="col-xs-2 "><img src="img/ht.png" alt="" class="img-thumbnail"></div>
+			<div class="col-xs-2 "><img src="img/css.png" alt="" class="img-thumbnail"></div>
+			<div class="col-xs-2 "><img src="img/js.png" alt="" class="img-thumbnail"></div>
+			<div class="col-xs-2 "><img src="img/jq.png" alt="" class="img-thumbnail"></div>
+			<div class="col-xs-2 "><img src="img/bs.png" alt="" class="img-thumbnail"></div>
+			<div class="col-xs-2 "><img src="img/wp.png" alt="" class="img-thumbnail"></div>
+		</div>	<!-- row -->
+		
+		
+	</div><!-- container -->
+		<footer class="wow fadeInUp">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-3">
+					<h4>Контактная информация</h4>
+					<ul class="list-unstyled">
+						<li>
+							<i class="fa fa-phone-square"></i>							
+							 +7 926 540 54 46
+						</li>
+						<li>
+							<i class="fa fa-skype"></i>
+							 a5405446
+						</li>
+						<li>
+							<i class="fa fa-envelope"></i>
+							<a href="mailto:skech@mail.ru"> skech@mail.ru</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-xs-3">
+					<h4>Я в соц.сетях</h4>
+					<ul class="list-unstyled">
+						<li>
+							<a href="https://vk.com/realkillingjoke">
+								<i class="fa fa-vk fa-2x"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-xs-3">
+					<h4>Прочие услуги</h4>
+					<ul class="list-unstyled">
+						<li>Создание сайтов "под-ключ"</li>
+						<li>SEO-оптимизация</li>
+						<li>Доработка и перенос сайтов</li>
+						<li>Поддержка и администрирование сайтов</li>
+					</ul>
+				</div>
+				<div class="col-xs-3">
+					<h4>
+						Доп.инфо
+					</h4>
+					<p>
+						Создано при помощи Bootstrap
+					</p>
+					<p>
+						Рефактор &copy; 2015
+					</p>
+				</div>
+			</div>
+			</div> <!-- container -->
+		</footer>
+	<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+	  
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/jquery.scrollUp.min.js"></script>
+	<script src="js/main.js"></script> 
+</body>
+</html>
