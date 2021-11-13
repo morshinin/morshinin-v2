a2202dcebf47153ed53193855040ee2e75be24c3
Mikhail Morshinin <314159265absurdus@gmail.com>
2021-11-13 13:36:27 +0300

    Delete credentials

--- example.php
+++ example.php
@@ -12,13 +12,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'billblake';                 // SMTP username
-$mail->Password = 'apQFihfnL3';                           // SMTP password
+$mail->Username = '';                 // SMTP username
+$mail->Password = '';                           // SMTP password
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
-$mail->setFrom('billblake@yandex.ru', 'Mailer');
-$mail->addAddress('skech@mail.ru', 'Joe User');     // Add a recipient
+$mail->setFrom('', 'Mailer');
+$mail->addAddress('', 'Joe User');     // Add a recipient
 // $mail->addAddress('ellen@example.com');               // Name is optional
 // $mail->addReplyTo('info@example.com', 'Information');
 // $mail->addCC('cc@example.com');
